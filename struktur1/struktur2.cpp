1dcc07fc1883036246bf1ad4243c4833b1deb9b7
Asyurazhr <Asyuradjola2004@gmail.com>
2024-04-02 17:56:26 +0700

    membuat program utama bagian 2

--- struktur1/struktur2.cpp
+++ struktur1/struktur2.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 struct DetailAlamat {
-
+	string desa;
+	string kota;
 };
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
+
+
+int main()
+{
+
+}
